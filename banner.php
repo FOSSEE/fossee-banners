1761228dfb0980faaaea5665bb4a4334a786ec8f
Sashi20 <gayatrirekha204@gmail.com>
2022-03-14 17:13:47 +0530

    First commit

---
+++ banner.php
@@ -0,0 +1,20 @@
+<?php
+$today = date("Y-m-d H:i:s");
+$r_date = "2022-03-05 23:59:59.0";
+//$mapathon_date = "2022-02-12 23:59:59.0";
+//$esim_marathon_date = "2022-02-24 23:59:59.0";
+if($today<$r_date){
+echo '  <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
+          <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
+           <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
+ <div style="margin-bottom:5px;border-bottom:none;width:90%">
+        
+         <a href="https://www.it.iitb.ac.in/nmeict/workshopContent.html?workshopid=HLL0wA0XvfFFGsG5WdQrLA&category=UubpVTjA3FS-DQx8uW4rlA" target="_blank" >  
+<img src="https://static.fossee.in/r/banners/web-bnr1.png">
+</a> 
+
+</div> 
+';
+
+}
+?>
