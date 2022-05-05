5699a6cb71a05d5203191167b20ed711548c0a98
Saketh1499 <saketh1499@gmail.com>
2022-05-05 12:32:40 +0530

    Updating python hackathon banner

--- banner.php
+++ banner.php
@@ -2,13 +2,13 @@
 $today = date("Y-m-d H:i:s");
 $p_date = "2022-05-20 23:59:59.0";
 if($today<$p_date){
-echo '  <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
+echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
            <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
 <div class="nice-bar" style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
         
 <a class="nice-text" href="https://hackathon.fossee.in/python/" target="_blank">  
-<img src="https://static.fossee.in/fellowship2022/web-bnr.gif">
+<img src="https://static.fossee.in/python-hackathon/banners/Web-bnr-python.gif">
 </a> 
 </div> 
 ';
