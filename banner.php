82285a78e73f95b992c371b2fef0b985e30fc9a8
Saketh1499 <saketh1499@gmail.com>
2022-05-05 11:23:17 +0530

    Adding python hackathon banner

--- banner.php
+++ banner.php
@@ -1,18 +1,14 @@
 <?php
 $today = date("Y-m-d H:i:s");
-$p_date = "2022-04-05 23:59:59.0";
-$fellowship_date = "2022-04-07 23:59:59.0";
+$p_date = "2022-05-20 23:59:59.0";
 if($today<$p_date){
 echo '  <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
            <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
 <div class="nice-bar" style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
         
-<a class="nice-text" href="https://www.it.iitb.ac.in/nmeict/workshopContent.html?workshopid=k_0GFfvxZUF2N13i7_uEcw&category=UubpVTjA3FS-DQx8uW4rlA" target="_blank">  
-<img src="https://static.fossee.in/python/banners/web-bnr.png">
-</a>
-<a class="nice-text" href="https://fossee.in/fellowship/2022" target="_blank" >  
-<img src="https://static.fossee.in/fellowship2022/web-bnr.gif"> 
+<a class="nice-text" href="https://hackathon.fossee.in/python/" target="_blank">  
+<img src="https://static.fossee.in/fellowship2022/web-bnr.gif">
 </a>
 </div> 
 ';
