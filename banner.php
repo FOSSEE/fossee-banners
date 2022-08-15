0209e54603ff3d22899d1119804c7b07f8a61b36
Sashi20 <gayatrirekha204@gmail.com>
2022-08-15 08:07:25 +0530

    Add independence day banner

--- banner.php
+++ banner.php
@@ -1,15 +1,14 @@
 <?php
 $today = date("Y-m-d H:i:s");
-$p_date = "2022-05-20 23:59:59.0";
+$p_date = "2022-08-15 23:59:59.0";
 if($today<$p_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
            <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
 <div style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
         
-<a href="https://hackathon.fossee.in/python/" target="_blank">  
-<img src="https://static.fossee.in/python-hackathon/banners/Web-bnr-python.gif">
-</a> 
+<img src="https://static.fossee.in/fossee/banners/har_ghar_tiranga.png">
+
 </div> 
 ';
  // class="nice-bar" class="nice-text"
