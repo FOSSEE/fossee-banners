1d984d5afcba7a5341453a0d0b8fb41a46efa285
Sashi20 <gayatrirekha204@gmail.com>
2023-01-18 11:12:16 +0530

    Update date for animation hackathon banner

--- banner.php
+++ banner.php
@@ -1,6 +1,6 @@
 <?php
 $today = date("Y-m-d H:i:s");
-$p_date = "2023-01-20 23:59:59.0";
+$p_date = "2023-02-05 23:59:59.0";
 if($today<$p_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
