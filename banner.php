5f908e112faa722cc3293456f418b487d62decb6
akshay9085 <akshaythakur9085@gmail.com>
2023-04-21 11:53:30 +0530

    new banner

--- banner.php
+++ banner.php
@@ -1,13 +1,13 @@
 <?php
 $today = date("Y-m-d H:i:s");
-$fellowship_date = "2023-04-14 23:59:59.0";
+$fellowship_date = "2023-04-27 23:59:59.0";
 if($today<$fellowship_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
            <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
 <div style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
     <a href="https://fossee.in/fellowship/2023" target="_blank">
-        <img src="https://static.fossee.in/fossee/banners/fsf-2023-banner-extended.png">
+        <img src="https://static.fossee.in/fossee/banners/web-bnr-static-new.png">
     </a>    
 
 </div> 
