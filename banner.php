6650a46c7ac7b055f3b85639f60488fe89ab8f8c
akshaythakur8764 <huntergaming2105@gmail.com>
2024-06-04 09:46:06 +0530

    new banner added

--- banner.php
+++ banner.php
@@ -1,7 +1,7 @@
 <?php
 $today = date("Y-m-d H:i:s");
 $hackathon_date = "2024-04-26 23:59:59.0";
-$fellowship_date = "2024-04-22 23:59:59.0";
+$fellowship_date = "2024-06-23 23:59:59.0";
 if($today<$fellowship_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
@@ -10,7 +10,7 @@ echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/ban
            <div style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
           
            <a href="https://animate2024.fossee.in/" target="_blank">
-               <img src="https://static.fossee.in/fossee/banners/web-bnr-animate.gif">
+               <img src="https://static.fossee.in/focal/banners/web-bnr-05.gif">
            </a>
        </div> 
        ';
