605109919f10280ba8d82d80083de581fedbe428
akshaythakur8764 <huntergaming2105@gmail.com>
2024-08-07 13:47:32 +0530

    qgis banner added

--- banner.php
+++ banner.php
@@ -1,16 +1,16 @@
 <?php
 $today = date("Y-m-d H:i:s");
 $hackathon_date = "2024-04-26 23:59:59.0";
-$fellowship_date = "2024-06-23 23:59:59.0";
+$fellowship_date = "2024-09-15 23:59:59.0";
 if($today<$fellowship_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
            <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
 
-           <div style="margin-bottom:5px; border-bottom:none; width:60%; background-color:transparent;">
+           <div style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
           
-           <a href="https://animate2024.fossee.in/" target="_blank">
-               <img src="https://static.fossee.in/focal/banners/web-bnr-05.gif">
+           <a href="https://iitb-mapathon-2024.fossee.in/" target="_blank">
+               <img src="https://static.fossee.in/qgis/banners/web-top-bnr.png>
            </a>
        </div> 
        ';
