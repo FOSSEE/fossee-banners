5d65f82ce9b9d0318fb55eed2270a3c520efc755
akshay <117985209+akshay9085@users.noreply.github.com>
2024-12-30 10:28:49 +0530

    adjust the first image of banner

--- banner.php
+++ banner.php
@@ -8,13 +8,13 @@ echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/ban
            <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
 
            <div class="nice-bar" style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
-           
+            <a class="nice-text" href="https://scilab-hackathon.fossee.in/" target="_blank">
+               <img src="https://static.fossee.in/scilab/banners/sci-webbnr-96.gif">
+           </a>   
             <a class="nice-text" href="https://fossee.in/semester-internship/2025" target="_blank">
                <img src="https://static.fossee.in/fossee/banners/three.gif">
            </a>
-           <a class="nice-text" href="https://scilab-hackathon.fossee.in/" target="_blank">
-               <img src="https://static.fossee.in/scilab/banners/sci-webbnr-96.gif">
-           </a>    
+           
            
            
        </div> 
