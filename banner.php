6106d3f0e92f8c68fd5ae2a7003dc25a8a699d02
nikitasabale96 <sabalenikita96@gmail.com>
2025-06-23 15:15:13 +0530

    Update banner.php

--- banner.php
+++ banner.php
@@ -3,14 +3,14 @@ $today = date("Y-m-d H:i:s");
 $hackathon_date = "2025-02-5 23:59:59.0";
 $fellowship_date = "2025-04-09 23:59:59.0";
 $animate_date = "2025-18-06 23:59:59.0";
-if($today<$fellowship_date){
+if($today<$animate_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
            <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
 
            <div class="nice-bar" style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
 
-          <a class="nice-text" href="https://animate2025.fossee.in/" target="_blank">
+          <a  href="https://animate2025.fossee.in/" target="_blank">
                <img src="https://static.fossee.in/fossee/banners/web-bnr-01.png">
            </a>
             <!--<a class="nice-text" href="https://scilab-hackathon.fossee.in/" target="_blank">
