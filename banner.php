81d7e474727def5658f58e68ff8cdd3db001f96c
nikitasabale96 <sabalenikita96@gmail.com>
2025-09-02 11:59:23 +0530

    Update banner.php

--- banner.php
+++ banner.php
@@ -3,16 +3,21 @@ $today = date("Y-m-d H:i:s");
 $hackathon_date = "2025-02-5 23:59:59.0";
 $fellowship_date = "2025-04-09 23:59:59.0";
 $animate_date = "2025-31-07 23:59:59.0";
-if($today<$animate_date){
+$autumn_date = "2025-15-09 23:59:59.0";
+if($today<$autumn_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
            <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
 
            <div class="nice-bar" style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
 
-          <a  href="https://animate2025.fossee.in/workshop/" target="_blank">
-          <!--<img src="https://static.fossee.in/focal/banners/web-bnr-count.gif">-->
+         <!-- <a  href="https://fossee.in/semester-internship/autumn/2025" target="_blank">
+          <img src="https://static.fossee.in/focal/banners/web-bnr-count.gif">
+           </a>-->
+           <a  href="https://animate2025.fossee.in/workshop/" target="_blank">
+          <img src="https://static.fossee.in/fossee/banners/webbnr.gif">
            </a>
+          
             <!--<a class="nice-text" href="https://scilab-hackathon.fossee.in/" target="_blank">
                 <img src="https://static.fossee.in/scilab/banners/sci-webbnr-96.gif">
             </a> -->  
