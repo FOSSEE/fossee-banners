302cba01f499d515b3a4951874c16689c573147f
nikitasabale96 <sabalenikita96@gmail.com>
2025-09-04 14:35:29 +0530

    Update banner.php to add esim_marathon banner

--- banner.php
+++ banner.php
@@ -4,6 +4,7 @@ $hackathon_date = "2025-02-5 23:59:59.0";
 $fellowship_date = "2025-04-09 23:59:59.0";
 $animate_date = "2025-31-07 23:59:59.0";
 $autumn_date = "2025-15-09 23:59:59.0";
+$ihp_marathon_date = "2025-08-10 23:59:59.0";
 if($today<$autumn_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
@@ -29,6 +30,25 @@ echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/ban
        ';
         // class="nice-bar" class="nice-text"
        }
+          if($today<$ihp_marathon_date){
+                    
+echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
+          <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
+           <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
+
+           <div class="nice-bar" style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
+
+           <a  href="https://esim-hackathon-2025.fossee.in/" target="_blank">
+          <img src="https://static.fossee.in/fossee/banners/eSim-webbnr.gif">
+           </a>
+           
+           
+           
+       </div> 
+       ';
+        // class="nice-bar" class="nice-text"
+       }
+
        
 
        /*else{
