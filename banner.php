c6b53569ef58c6148999ad9c5f78cd59a4b1c26a
nikitasabale96 <sabalenikita96@gmail.com>
2025-09-16 11:39:57 +0530

    Update banner.php for SLI Autumn-2025

--- banner.php
+++ banner.php
@@ -3,8 +3,8 @@ $today = date("Y-m-d H:i:s");
 $hackathon_date = "2025-02-5 23:59:59.0";
 $fellowship_date = "2025-04-09 23:59:59.0";
 $animate_date = "2025-31-07 23:59:59.0";
-$autumn_date = "2025-15-09 23:59:59.0";
-$ihp_marathon_date = "2025-08-10 23:59:59.0";
+$autumn_date = "2025-21-09 23:59:59.0";
+$ihp_marathon_date = "2025-14-10 23:59:59.0";
 if($today<$autumn_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
@@ -14,7 +14,7 @@ echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/ban
 
            
            <a class="nice-text" href="https://fossee.in/Semester-Internship/Autumn/2025" target="_blank">
-          <img src="https://static.fossee.in/fossee/banners/webbnr.gif">
+          <img src="https://static.fossee.in/fossee/banners/Webbnr-05.gif">
            </a>
 
            <a class="nice-text" href="https://esim-hackathon-2025.fossee.in/" target="_blank">
