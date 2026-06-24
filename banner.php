268e9631d04710e557c5f5b35ceb5c756a040634
nikitasabale96 <sabalenikita96@gmail.com>
2026-06-24 11:32:36 +0530

    Update banner links and add Scilab hackathon date

--- banner.php
+++ banner.php
@@ -3,16 +3,16 @@ $today = date("Y-m-d H:i:s");
 $hackathon_date = "2025-02-5 23:59:59.0";
 $autumn_date = "2025-10-27 23:59:59.0";
 $semester_internship_date = "2026-02-10 23:59:59.0";
-$arduino_day = "2026-04-13 23:59:59.0";
-if($today<$arduino_day){
+$scilab_hackathon_date = "2026-07-15 23:59:59.0";
+if($today<$scilab_hackathon_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
            <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
 
            <div  style="margin-bottom:5px; border-bottom:none; width:90%; background-color:transparent;">
 
-       <a  href="https://fossee.in/fellowship/2026" target="_blank">
-          <img src="https://static.fossee.in/fossee/banners/fosBnrSumrFellos-202603-v01.gif">
+       <a  href="https://scilab-gui-hackathon.fossee.in/" target="_blank">
+          <img src="https://static.fossee.in/fossee/banners/slbWebbnr01.gif">
            </a> 
 
        
